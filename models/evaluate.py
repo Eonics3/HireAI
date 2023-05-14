2b77c50ef706c447510a7e6c9ee5f6ff6eac41db
golanruang <60362693+golanruang@users.noreply.github.com>
2023-05-13 19:47:12 -0700

    starting sentiment analysis

--- models/evaluate.py
+++ models/evaluate.py
@@ -1,11 +1,10 @@
-# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
 import openai
 
-def evaluate():
+def evaluate_content():
     f = open("transcript.txt", 'r')
     transcript = f.readlines()
 
-    openai.api_key = "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"
+    openai.api_key = "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"
 
     f2 = open('resume.txt', 'r')
     resume = f2.readlines()
@@ -13,14 +12,16 @@ def evaluate():
     f3 = open('questions.txt', 'r')
     questions = f3.readlines()
 
+    f4 = open('job_info.txt', 'r')
+    job_info = f4.readlines()
+
     response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-            {"role": "system", "content": "You are giving feedback on how users can improve their interviewing skills for a software engineer internship. Write the feedback without using first-person perspective."},
-            {"role": "user", "content": "Here's my resume: %s. Here is my transcript %s. Please give constructive and concise feedback on my \
-    interview responses to the following questions: %s" % (resume, transcript, questions)},
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Give feedback on how users can improve their interviewing skills for a software engineer internship at %s. Write the feedback without using first-person perspective." % job_info},
+            {"role": "user", "content": "Here's my resume: %s. Here is my transcript %s. Please list constructive and concise feedback on my interview responses to the following questions: %s." % (resume, transcript, questions)},
         ]
     )
 
-    response_message = response['choices'][0]['message']['content']
-    print(response_message)
+    response = response['choices'][0]['message']['content']
+    print(response)
